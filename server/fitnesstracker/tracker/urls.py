a112da30aa1b02e3b17f44f7b0f957e94508dac2
VINANK <neupaneayush3@gmail.com>
2024-02-29 17:47:30 +0545

    added apis for workout

--- server/fitnesstracker/tracker/urls.py
+++ server/fitnesstracker/tracker/urls.py
@@ -4,6 +4,8 @@ from rest_framework.routers import DefaultRouter
 
 router=DefaultRouter()
 router.register('exercise',views.ExerciseViewSet)
+router.register('workout',views.WorkoutViewSet)
+router.register('workoutexercise',views.WorkoutExerciseViewSet)
 
 urlpatterns=[
 	path('index',views.index,name='index'),
